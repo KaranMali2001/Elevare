8c9f12f81d5dd9b03e001bf32f998eee45d0690c
karan <karanmali122001@gmail.com>
2025-01-15 02:54:30 +0530

    updated banner

--- nextapp/src/app/layout.tsx
+++ nextapp/src/app/layout.tsx
@@ -1,18 +1,17 @@
-"use client";
 import { Toaster } from "react-hot-toast";
 import "react-loading-skeleton/dist/skeleton.css";
 import "./globals.css";
 import { Providers } from "./providers";
 
-import { useEffect, useState } from "react";
-import Banner from "@/components/banner";
-// export const metadata: Metadata = {
-//   title: "elevareapp",
-//   description: "Elevate the way of handling your mails with elevareapp",
-//   other: {
-//     "google-site-verification": "du5i7A2AeolTZjy9AXJXKcWd0B8g_wG9MLH0h6_DPQ0",
-//   },
-// };
+import { FloatingBanner } from "@/components/banner";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "elevareapp",
+  description: "Elevate the way of handling your mails with elevareapp",
+  other: {
+    "google-site-verification": "du5i7A2AeolTZjy9AXJXKcWd0B8g_wG9MLH0h6_DPQ0",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -23,8 +22,8 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body style={{ fontFamily: "SFProDisplayMedium" }}>
         <Providers>
-          <Banner />
-          {children}
+          <FloatingBanner />
+          <div className="pt-1">{children}</div>
           <Toaster
             position="top-center"
             gutter={12}
